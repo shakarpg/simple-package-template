c6b835b7000cfe83f42576c94d8ecd5d7fb6398a
shakarpg <101528402+shakarpg@users.noreply.github.com>
2025-04-24 14:44:19 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="package_shakarpg",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="shakarpg",
+    author_email="rafinharpg@gmail.com",
+    description="projeto pypi",
     long_description=page_description,
     long_description_content_type="text/markdown",
     url="my_github_repository_project_link"
